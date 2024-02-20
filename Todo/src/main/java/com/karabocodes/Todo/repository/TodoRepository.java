2d50f7ae5bf57e33a627faa655712bc56b87184b
karabocodes <145660355+karabocodes@users.noreply.github.com>
2024-02-20 16:58:52 +0200

    Delete Todo directory

--- Todo/src/main/java/com/karabocodes/Todo/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.karabocodes.Todo.repository;
-
-import com.karabocodes.Todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository  extends JpaRepository<Todo, Long> {
-
-}
